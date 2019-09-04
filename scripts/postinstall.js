78e05c712ce221b70bab15be28f3d66e5a196989
Jerome Czachor <jerome.czachor>
2019-09-04 09:57:09 -0400

    postinstall script fix, ensure the themes and files are updated

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -6,17 +6,32 @@ const npm = require('npm');
 
 const MODULES_DIR = path.join(__dirname, "..", "node_modules")
 const LIB_DIR = path.join(__dirname, "..", "lib")
+const package = require('../package.json')
 
-if (!fs.pathExistsSync(MODULES_DIR)) {
-  npm.load(function(err) {
-    // handle errors
-    npm.commands.install()
-  })
+
+let pkgs = []
+// console.log(package)
+for (let key of ["dependencies", "devDependencies"]) {
+  if (key in package) {
+    pkgs.push(...Object.keys(package[key]).map(k => k+"@"+package[key][k]))
+  }
 }
+
+console.log(pkgs.join(" "))
+/*
+// console.log(pkgs)
+npm.load(function(err) {
+  // handle errors
+  //npm.install(pkgs)
+  npm.ls(pkgs, (er, data, lite) => {
+    console.log(Object.keys(data))
+  })
+})
 
 if (fs.pathExistsSync(MODULES_DIR) && !fs.pathExistsSync(LIB_DIR)) {
   npm.load(function(err) {
     // handle errors
-    npm.commands.run(['build'])
+    npm.run(['build'])
   })
 }
+*/
